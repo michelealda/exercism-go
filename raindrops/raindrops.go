0fe9ca7cc667de80875537257da82b7d9507374c
michelealda <michele.alda@gmail.com>
2019-01-30 13:30:43 +0100

    Simpliefied raindrop solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -2,33 +2,20 @@ package raindrops
 
 import "strconv"
 
-type factor struct {
-	divisor int
-	word    string
-}
-
 //Convert a
 func Convert(n int) string {
-	divisible := func(x, d int, s string) string {
-		if x%d == 0 {
-			return s
-		}
-		return ""
-	}
-
-	vocabulary := []factor{
-		{3, "Pling"},
-		{5, "Plang"},
-		{7, "Plong"},
-	}
-
 	result := ""
-	for _, x := range vocabulary {
-		result += divisible(n, x.divisor, x.word)
+	if n%3 == 0 {
+		result += "Pling"
 	}
-
-	if result != "" {
-		return result
+	if n%5 == 0 {
+		result += "Plang"
 	}
-	return strconv.Itoa(n)
+	if n%7 == 0 {
+		result += "Plong"
+	}
+	if result == "" {
+		return strconv.Itoa(n)
+	}
+	return result
 }

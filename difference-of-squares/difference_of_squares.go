b2129cddb00f5ae0171edd4f47b1740cfe75ba2d
michelealda <michele.alda@gmail.com>
2019-02-11 09:34:21 +0100

    Improved solution for difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -7,11 +7,8 @@ func SquareOfSum(n int) (sum int) {
 }
 
 //SumOfSquares calculates sum of the squares of the first n natural number
-func SumOfSquares(n int) (sum int) {
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 //Difference calculates the difference between SquareOfSum and SumOfSquares

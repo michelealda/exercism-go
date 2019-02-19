6236deeb72955f16a9167e7cdc5cd16926290024
Michele Alda <MAlda@binck.nl>
2019-02-19 10:22:05 +0100

    Refactored Luhn solution

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -5,21 +5,26 @@ import (
 	"strings"
 )
 
+func isSecondDigit(n string, i int) bool {
+	return len(n)%2 == 0 && i%2 == 0 ||
+		len(n)%2 == 1 && i%2 == 1
+}
+
 //Valid calculate the Luhn checksum of a string of digits
 func Valid(s string) bool {
 	sum := 0
-	n := strings.Join(strings.Fields(s), "")
-	if len(n) < 2 {
+	s = strings.Replace(s, " ", "", -1)
+	if len(s) < 2 {
 		return false
 	}
-	even := func(k int) bool { return k%2 == 0 }
-	for i := range n {
-		x, err := strconv.Atoi(string(n[i]))
+
+	for i := range s {
+		x, err := strconv.Atoi(string(s[i]))
 		if err != nil {
 			return false
 		}
 		tosum := x
-		if (even(len(n)) && even(i)) || (!even(len(n)) && !even(i)) {
+		if isSecondDigit(s, i) {
 			tosum = (x * 2)
 			if tosum > 9 {
 				tosum -= 9
